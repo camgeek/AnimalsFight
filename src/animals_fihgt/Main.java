e1ab6e8c64de232f72d4de745bf85360af4a531c
cvrignaud <cvrignaud@softia.fr>
2018-01-09 10:18:33 +0100

    Add ArrayList

--- src/animals_fihgt/Main.java
+++ src/animals_fihgt/Main.java
@@ -1,4 +1,5 @@
 package animals_fihgt;
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args)
@@ -8,12 +9,16 @@ public class Main {
 		Dog chien = new Dog();
 		Ferret furet = new Ferret();
 		attack_rand(chien, furet);
-		System.out.println(chat.getLife());
-		System.out.println(chien.getLife());
-		System.out.println(furet.getLife());
-		System.out.println(chat.getAttack());
-		System.out.println(chien.getAttack());
-		System.out.println(furet.getAttack());
+		ArrayList<Animal> animals = new ArrayList<Animal>();
+		
+		animals.add(chat);
+		animals.add(chien);
+		animals.add(furet);
+		
+		for (Animal object: animals) {
+			System.out.println(object.getLife());
+			System.out.println(object.getAttack());
+		}
 	}
 	
 	static public void attack_rand(Animal ani1,Animal ani2)
